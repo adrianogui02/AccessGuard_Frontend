70c7107c7d4a9d1bcf45794bc46cdf04d0a0dbce
Adriano Nobre <adrianonobrevg02@gmail.com>
2024-05-14 21:58:43 -0400

    update logic

--- src/components/InviteSuccess/InviteSuccess.jsx
+++ src/components/InviteSuccess/InviteSuccess.jsx
@@ -21,7 +21,50 @@ const InviteSuccess = () => {
 
         if (response.data.active) {
           // Chame a função para desativar o QR Code se o convite está ativo
+
+          const deactivateQRCode = async (uuid) => {
+            try {
+              await axios.post(
+                `https://accessguardbackend-production.up.railway.app/api/invite/desactive/${uuid}`
+              );
+              console.log("QR Code desativado com sucesso!");
+            } catch (error) {
+              console.error("Erro ao desativar QR Code:", error);
+            }
+          };
+
           deactivateQRCode(uuid);
+
+          if (!invitation) {
+            return (
+              <div className="invite-details-background">
+                <div className="blur-effect"></div>
+                <div>Carregando detalhes do convite...</div>
+              </div>
+            );
+          }
+
+          return (
+            <div className="invite-details-background">
+              <div className="invite-details-container">
+                <img src={logo} className="invite-details-logo" alt="Logo" />
+                <div className="invite-details-content">
+                  <img
+                    src={OkIcon}
+                    alt="QR Code do Convite"
+                    className="invite-success-icon"
+                  />
+                  <p className="invite-detail">Escaneamento bem-sucedido!</p>
+                  <p className="invite-detail">
+                    <strong>
+                      Parabéns! Você escaneou com sucesso o QR code e seu acesso
+                      foi autorizado.
+                    </strong>
+                  </p>
+                </div>
+              </div>
+            </div>
+          );
         } else {
           // Redirecionar se o convite não estiver ativo
           navigate(`/QRCode/Failed/${uuid}`); // Assumindo que você tem uma rota '/error' para convites inativos
@@ -33,48 +76,6 @@ const InviteSuccess = () => {
 
     fetchInvitation();
   }, [uuid, navigate]);
-
-  const deactivateQRCode = async (uuid) => {
-    try {
-      await axios.post(
-        `https://accessguardbackend-production.up.railway.app/api/invite/desactive/${uuid}`
-      );
-      console.log("QR Code desativado com sucesso!");
-    } catch (error) {
-      console.error("Erro ao desativar QR Code:", error);
-    }
-  };
-
-  if (!invitation) {
-    return (
-      <div className="invite-details-background">
-        <div className="blur-effect"></div>
-        <div>Carregando detalhes do convite...</div>
-      </div>
-    );
-  }
-
-  return (
-    <div className="invite-details-background">
-      <div className="invite-details-container">
-        <img src={logo} className="invite-details-logo" alt="Logo" />
-        <div className="invite-details-content">
-          <img
-            src={OkIcon}
-            alt="QR Code do Convite"
-            className="invite-success-icon"
-          />
-          <p className="invite-detail">Escaneamento bem-sucedido!</p>
-          <p className="invite-detail">
-            <strong>
-              Parabéns! Você escaneou com sucesso o QR code e seu acesso foi
-              autorizado.
-            </strong>
-          </p>
-        </div>
-      </div>
-    </div>
-  );
 };
 
 export default InviteSuccess;
