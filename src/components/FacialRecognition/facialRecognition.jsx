050e7cfa6eed942dbae64c8b3cf856b00274ecad
Adriano Nobre <adrianonobrevg02@gmail.com>
2024-05-15 08:54:31 -0400

    Update facialRecognition.jsx

--- src/components/FacialRecognition/facialRecognition.jsx
+++ src/components/FacialRecognition/facialRecognition.jsx
@@ -21,7 +21,7 @@ function FacialRecognition() {
 
   React.useEffect(() => {
     const loadModels = async () => {
-      const MODEL_URL = "/models";
+      const MODEL_URL = "/Models";
       await Promise.all([
         faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
         faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
